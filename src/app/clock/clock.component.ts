c83e4f5b249c4d651a8855ed13c305227c91465d
TylerMizuyabu <mizuyabu@hotmail.ca>
2016-12-15 00:20:08 -0500

    now will calculate proper value for next christmas, and any date past 24th

--- src/app/clock/clock.component.ts
+++ src/app/clock/clock.component.ts
@@ -20,19 +20,28 @@ export class ClockComponent implements OnInit {
     setInterval(() => {
       this.update();
       }, 1000);
-    let start = new Date();
-    this.months = this.target.getMonth()-start.getMonth();
-    this.days = this.target.getDate()-start.getDate();
-    let count = (this.target.getTime()-start.getTime())/1000;
-    this.hours = Math.floor(count/(60*60))%24;
-    this.minutes = Math.floor(count/(60))%60;
-    this.seconds = Math.floor(count)%60;
+    this.update();
   }
 
   update() {
     let start = new Date();
-    this.months = this.target.getMonth()-start.getMonth();
-    this.days = this.target.getDate()-start.getDate();
+    this.months = this.target.getMonth() - start.getMonth();
+    this.days = this.target.getDate() - start.getDate();
+    if (this.days < 0) {
+      if (this.months === 0) {
+        if (this.days === -1) {
+          start = this.target;
+          this.days = 0;
+        }else {
+          this.days = 31 - start.getDate() + 24;
+          this.months = 11;
+          this.target.setFullYear(this.target.getFullYear()+1);
+        }
+      }else {
+        this.months --;
+      }
+
+    }
     let count = (this.target.getTime()-start.getTime())/1000;
     this.hours = Math.floor(count/(60*60))%24;
     this.minutes = Math.floor(count/(60))%60;
